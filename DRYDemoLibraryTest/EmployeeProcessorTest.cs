f5c477a789957914f6dc43c75f152a05c9525395
Lynx82 <ristanovski@hotmail.com>
2022-03-20 14:27:47 +0100

    Created first xunit test to check the GenerateEmployeeId method.

---
+++ DRYDemoLibraryTest/EmployeeProcessorTest.cs
@@ -0,0 +1,27 @@
+﻿using DRYDemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DRYDemoLibraryTest
+{
+    public class EmployeeProcessorTest
+    {
+        [Theory]
+        [InlineData("John", "Smith", "JohnSmit")]
+        public void GenerateEmployeeId_ShouldCalculate(string firstName, string lastName, string expectedStart)
+        {
+            // Arrange
+            EmployeeProcessor processor = new EmployeeProcessor();
+
+            // Act
+            string actualStart = processor.GenerateEmployeeId(firstName, lastName).Substring(0, 8);
+
+            // Assert
+            Assert.Equal(expectedStart, actualStart);
+        }
+    }
+}
